6bfa1c3957a608a9c4e35429bf9c22774ab41717
Aksharakeerthi <akshara2223@gmail.com>
2017-07-12 18:03:06 +0530

    Create oddtwo.java

---
+++ oddtwo.java
@@ -0,0 +1,16 @@
+# oddtwo
+import java.io.*
+import java.util.*;
+public class oddtwo{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int i,k;
+System.out.println("Enter the number");
+k=sc.nextInt();
+for(i=1;i<=100;i++){
+if(i%2==0){
+System.out.println(i);
+}
+}
+}
+}
